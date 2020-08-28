79335946716c07d84728c119d62c17416048fc05
Gustavo Lombardi <gustavolomabardi@WP061760NRJBA.local>
2020-08-28 00:46:19 -0300

    migração para o netlify - environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  API_URI: "https://tqsop-api.herokuapp.com",
+  //API_URI: "https://tqsop-api.herokuapp.com",
+  API_URI: "https://tqsop-api.netlify.app/.netlify/functions/server",
   theme: "slate"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@
 export const environment = {
   production: false,
   //API_URI: "http://localhost:3000",
-  API_URI: "https://tqsop-api.herokuapp.com",
+  //API_URI: "https://tqsop-api.herokuapp.com",
+  API_URI: "https://tqsop-api.netlify.app/.netlify/functions/server",
   theme: "slate"
 };
