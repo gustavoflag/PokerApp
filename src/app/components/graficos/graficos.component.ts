bcb7ed1abd0cbb32a8a006694dc364b6d68a0133
Gustavo Lombardi <gustavolomabardi@WP061760NRJBA.local>
2020-12-23 16:25:33 -0300

    Ajustes angular 10

--- src/app/components/graficos/graficos.component.ts
+++ src/app/components/graficos/graficos.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ConfigService } from '../../services/config.service';
 import { JogadorService } from '../../services/jogador.service';
 import { Globals } from '../../app.globals';
-//import { ChartDataSets, ChartOptions } from 'chart.js';
-//import { Color } from 'ng2-charts';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color } from 'ng2-charts';
 import { JogoService } from '../../services/jogo.service';
 import { ErrorHelper } from '../../helpers/error.helper';
 
@@ -18,32 +18,32 @@ export class GraficosComponent implements OnInit {
   quantidadeJogos: number;
   qtdSelecionados: number = 0;
 
-  // public lineChartData: ChartDataSets[] = [{ data: [], label: '', lineTension: 0 }];
-  // public lineChartLabels: any[] = [];
+  public lineChartData: ChartDataSets[] = [{ data: [], label: '', lineTension: 0 }];
+  public lineChartLabels: any[] = [];
 
-  // public lineChartOptions: (ChartOptions) = {
-  //   responsive: true,
-  //   scales: {
-  //     yAxes: [{
-  //        scaleLabel: {
-  //           display: true,
-  //           labelString: 'Pontos'
-  //        }
-  //     }],
-  //     xAxes: [{
-  //       scaleLabel: {
-  //          display: true,
-  //          labelString: 'Etapa'
-  //       }
-  //    }]
-  //  }
-  // };
-  // public lineChartColors: Color[] = [
+  public lineChartOptions: (ChartOptions) = {
+    responsive: true,
+    scales: {
+      yAxes: [{
+         scaleLabel: {
+            display: true,
+            labelString: 'Pontos'
+         }
+      }],
+      xAxes: [{
+        scaleLabel: {
+           display: true,
+           labelString: 'Etapa'
+        }
+     }]
+   }
+  };
+  public lineChartColors: Color[] = [
     
-  // ];
-  // public lineChartLegend = false;
-  // public lineChartType = 'line';
-  // public lineChartPlugins = [];
+  ];
+  public lineChartLegend = false;
+  public lineChartType = 'line';
+  public lineChartPlugins = [];
   // //
 
   cores = [
@@ -73,70 +73,70 @@ export class GraficosComponent implements OnInit {
   }
 
   selecionaJogador(jogador){
-    // if (!jogador.selecionado && this.qtdSelecionados == 10){
-    //   alert('Máximo de 10 jogadores por gráfico');
-    //   return;
-    // }
+    if (!jogador.selecionado && this.qtdSelecionados == 10){
+      alert('Máximo de 10 jogadores por gráfico');
+      return;
+    }
 
-    // jogador.selecionado = !jogador.selecionado;
+    jogador.selecionado = !jogador.selecionado;
 
-    // this.lineChartLabels = [];
-    // var pontuacoes = [];
+    this.lineChartLabels = [];
+    var pontuacoes = [];
 
-    // for (var i = 0; i <= this.quantidadeJogos; i++){
-    //   this.lineChartLabels.push(i.toString());
-    // }
+    for (var i = 0; i <= this.quantidadeJogos; i++){
+      this.lineChartLabels.push(i.toString());
+    }
 
-    // if (jogador.selecionado){
-    //   var cor = this.cores[Math.floor(Math.random() * this.cores.length)];
+    if (jogador.selecionado){
+      var cor = this.cores[Math.floor(Math.random() * this.cores.length)];
 
-    //   this.cores.splice(this.cores.indexOf(cor), 1);
+      this.cores.splice(this.cores.indexOf(cor), 1);
 
-    //   jogador.corGrafico = cor;
+      jogador.corGrafico = cor;
 
-    //   for (var i = 0; i <= this.quantidadeJogos; i++){
-    //     var etapaIndex = jogador.pontuacaoEtapas.map(function(e){ return e.etapa }).indexOf(i);
-    //     var etapaSelecionada = jogador.pontuacaoEtapas[etapaIndex];
-    //     var pontosEtapa = 0;
-    //     if (!etapaSelecionada){
-    //       if (pontuacoes.length > 0){
-    //         pontosEtapa = pontuacoes[pontuacoes.length - 1];
-    //       }
-    //     } else {
-    //       pontosEtapa = etapaSelecionada.pontos;
-    //     }
+      for (var i = 0; i <= this.quantidadeJogos; i++){
+        var etapaIndex = jogador.pontuacaoEtapas.map(function(e){ return e.etapa }).indexOf(i);
+        var etapaSelecionada = jogador.pontuacaoEtapas[etapaIndex];
+        var pontosEtapa = 0;
+        if (!etapaSelecionada){
+          if (pontuacoes.length > 0){
+            pontosEtapa = pontuacoes[pontuacoes.length - 1];
+          }
+        } else {
+          pontosEtapa = etapaSelecionada.pontos;
+        }
 
-    //     pontuacoes.push(pontosEtapa);
-    //   }
+        pontuacoes.push(pontosEtapa);
+      }
 
-    //   this.lineChartColors.push({
-    //     borderColor: cor,
-    //     backgroundColor: 'rgba(255,0,0,0)',
-    //   });
+      this.lineChartColors.push({
+        borderColor: cor,
+        backgroundColor: 'rgba(255,0,0,0)',
+      });
   
-    //   if (!this.lineChartData[0].data || this.lineChartData[0].data.length == 0){
-    //     this.lineChartData[0] = { data: pontuacoes, label: jogador.nome, lineTension: 0 };
-    //   } else {
-    //     this.lineChartData.push({ data: pontuacoes, label: jogador.nome, lineTension: 0 });
-    //   }
+      if (!this.lineChartData[0].data || this.lineChartData[0].data.length == 0){
+        this.lineChartData[0] = { data: pontuacoes, label: jogador.nome, lineTension: 0 };
+      } else {
+        this.lineChartData.push({ data: pontuacoes, label: jogador.nome, lineTension: 0 });
+      }
 
-    //   this.lineChartLegend = true;
-    //   this.qtdSelecionados++;
-    // } else {
-    //   if (this.lineChartData.length > 1){
-    //     const index = this.lineChartData.map(function(e) { return e.label; }).indexOf(jogador.nome);
-    //     this.lineChartData.splice(index, 1);
-    //   } else {
-    //     this.lineChartData[0] = { data: [], label: '', lineTension: 0 };
-    //     this.lineChartLegend = false;
-    //   }
+      this.lineChartLegend = true;
+      this.qtdSelecionados++;
+    } else {
+      if (this.lineChartData.length > 1){
+        const index = this.lineChartData.map(function(e) { return e.label; }).indexOf(jogador.nome);
+        this.lineChartData.splice(index, 1);
+      } else {
+        this.lineChartData[0] = { data: [], label: '', lineTension: 0 };
+        this.lineChartLegend = false;
+      }
 
-    //   const indexCor = this.lineChartColors.map(function(e) { return e.borderColor; }).indexOf(jogador.corGrafico);
-    //   this.lineChartColors.splice(indexCor, 1);
-    //   this.cores.push(jogador.corGrafico);
-    //   jogador.corGrafico = "#eeeeee";
-    //   this.qtdSelecionados--;
-    // }
+      const indexCor = this.lineChartColors.map(function(e) { return e.borderColor; }).indexOf(jogador.corGrafico);
+      this.lineChartColors.splice(indexCor, 1);
+      this.cores.push(jogador.corGrafico);
+      jogador.corGrafico = "#eeeeee";
+      this.qtdSelecionados--;
+    }
     
   }
 
@@ -144,10 +144,10 @@ export class GraficosComponent implements OnInit {
     this.jogoService.quantidade().subscribe(qtd => { 
       this.quantidadeJogos = qtd;
 
-      // this.lineChartLabels = [];
-      // for (var i = 0; i <= this.quantidadeJogos; i++){
-      //   this.lineChartLabels.push(i.toString());
-      // }
+      this.lineChartLabels = [];
+      for (var i = 0; i <= this.quantidadeJogos; i++){
+        this.lineChartLabels.push(i.toString());
+      }
     }, error => this.errorHelper.handle(error));
   }
 
