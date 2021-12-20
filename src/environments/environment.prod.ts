516a75acfa6bdf24bba7c14031b2430faf15345c
Gustavo Lombardi <gustavo.bandeira@iteris.com.br>
2021-12-20 16:24:24 -0300

    Ajustes v2021

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   //API_URI: "https://tqsop-api.herokuapp.com",
-  API_URI: "https://tqsop-api.netlify.app/.netlify/functions/server",
+  API_URI: "https://tqsop2021-api.netlify.app/.netlify/functions/server",
   API_RELOGIO_URI: "https://tqsop-clock-api.herokuapp.com",
   theme: "slate"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: false,
   //API_URI: "http://localhost:3000",
   //API_RELOGIO_URI: "http://localhost:3000",
-  API_URI: "https://tqsop-api.netlify.app/.netlify/functions/server",
+  API_URI: "https://tqsop2021-api.netlify.app/.netlify/functions/server",
   API_RELOGIO_URI: "https://tqsop-clock-api.herokuapp.com",
   theme: "slate"
 };
