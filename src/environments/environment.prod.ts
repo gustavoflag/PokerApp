3411b46bbfe543466e308c816f64cf86e6f175ab
gustavoflag <gustavoflag@yahoo.com.br>
2025-01-02 01:37:03 -0300

    v2024

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   //API_URI: "https://tqsop-api.herokuapp.com",
-  API_URI: "https://tqsop-api.netlify.app/.netlify/functions/server",
+  API_URI: "https://tqsop2024-api.netlify.app/.netlify/functions/server",
   API_RELOGIO_URI: "https://poker-clock-api{index}.netlify.app/.netlify/functions",
   API_AUTH_URI: "https://auth-service-api.netlify.app/.netlify/functions/server",
   theme: "slate"
